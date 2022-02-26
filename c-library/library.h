889fc83ee03a377ae204f35f6f6dd5e8799c7aa8
dcseifert <daniel.seifert@unbasical.com>
2022-02-26 12:23:13 +0100

    Added first example functions to c-lib

--- c-library/library.h
+++ c-library/library.h
@@ -1,6 +1,9 @@
 #ifndef NATIVE_INTERFACES_LIBRARY_H
 #define NATIVE_INTERFACES_LIBRARY_H
 
-void hello(void);
+typedef void(*DelayedGreeting)(char*, char*);
+
+void greet(char *firstname, char *lastname);
+void greetDelayed(const DelayedGreeting pfn);
 
 #endif //NATIVE_INTERFACES_LIBRARY_H
