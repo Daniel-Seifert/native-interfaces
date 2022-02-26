30e54e6f2551f0e208c0571d714861419c6d45b6
Anton Saatze <a.saatze@googlemail.com>
2022-02-26 17:58:43 +0100

    use native lib, update mutliplatform version (fixes bug regarding pthread)

--- knative/src/nativeMain/kotlin/Main.kt
+++ knative/src/nativeMain/kotlin/Main.kt
@@ -1,5 +1,47 @@
-import nativelib.hello
+import kotlinx.cinterop.*
+import nativelib.DelayedGreeting
+import nativelib.Person
+import platform.posix.sleep
 
 fun main() {
-    hello()
+    memScoped {
+
+        "Trigger Callback in C".printBanner()
+        val callback: DelayedGreeting =
+            staticCFunction { firstname: CPointer<ByteVarOf<Byte>>?, lastname: CPointer<ByteVarOf<Byte>>? ->
+                "Callback from C".printBanner()
+                println("Async greetings to : ${firstname?.toKString()} ${lastname?.toKString()}")
+            }
+        nativelib.greetDelayed(callback)
+
+        "Call C function from Kotlin native".printBanner()
+        nativelib.greet("Black".cstr, "Widow".cstr)
+
+        "Passing array to C".printBanner()
+        val persons = listOf(
+            "Peter".cstr.ptr to "Parker".cstr.ptr,
+            "Tony".cstr.ptr to "Stark".cstr.ptr
+        )
+        val array = allocArray<Person>(persons.size)
+        persons.forEachIndexed { index, person ->
+            array[index].firstname = person.first
+            array[index].lastname = person.second
+        }
+        nativelib.greetAll(array, persons.size)
+
+        "Passing C array to Kotlin native".printBanner()
+        /*nativelib.getPersonsToGreet()
+        receiveStructArrayFromC(library).forEach {
+            println("Received person: ${it.firstname}, ${it.lastname}")
+        }*/
+
+        // Sleep to assure callback is invoked
+        sleep(2)
+    }
+}
+
+fun String.printBanner() {
+    val banner = "#".repeat(10)
+    println("\n$banner $this")
+    println("$banner${"#".repeat(this.length + 1)}")
 }
